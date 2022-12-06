d114f2a538a2843477a38aca9e1bb044aaa0cd0a
Nguyen Nguyen <khoinguyencubin@gmail.com>
2022-12-06 12:44:53 +0700

    Routes init

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const exp = require('constants')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({
@@ -26,26 +28,8 @@ app.engine('hbs', hbs.engine({
 app.set('view engine', 'hbs')
 app.set('views', './src/resources/views')
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/news/abcd', (req, res) => {
-  res.render('abcd')
-})
-
-app.get('/search', (req, res) => {
-  res.render('search')
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.send('')
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
