6b97867a6a0a92bff5583e6b5290a4b673269d95
Nguyen Nguyen <khoinguyencubin@gmail.com>
2022-12-06 13:18:47 +0700

    Route init (cont.)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,12 @@
 const newsRouter = require('./news')
-
+const siteRouter = require('./site')
 function route(app) {
-    app.get('/', (req, res) => {
-        res.render('home')
-    })
-    
-    // app.get('/news', (req, res) => {
-    //   res.render('news')
-    // })
+   
 
     app.use('/news', newsRouter)
+
+    app.use('/', siteRouter)
     
-    app.get('/news/abcd', (req, res) => {
-      res.render('abcd')
-    })
-    
-    app.get('/search', (req, res) => {
-      res.render('search')
-    })
-    
-    app.post('/search', (req, res) => {
-      console.log(req.body)
-      res.send('')
-    })
 }
 
 module.exports = route;
